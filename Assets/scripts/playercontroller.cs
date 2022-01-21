0dc574598bc3bbd4f3d68a68101958005c094a66
pramodaradhya <46969756+pramodaradhya@users.noreply.github.com>
2022-01-21 15:12:26 +0530

    keyboard control

---
+++ Assets/scripts/playercontroller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    public Animator animator;
+    private void Awake()
+    {
+        Debug.Log("player ccontroller is awake");
+    }
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     Debug.Log("collision:" + collision.gameObject.name);
+    // }
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }else if(speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
